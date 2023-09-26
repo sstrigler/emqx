99bdccef74eabab42054daf334b4fe79a72721af
Stefan Strigler <stefan.strigler@emqx.io>
2023-09-26 14:13:29 +0200

    fix: try fix version

--- .devcontainer/emqx-otp/install.sh
+++ .devcontainer/emqx-otp/install.sh
@@ -2,29 +2,30 @@
 
 set -e
 
-source ./library_scripts.sh
+#source ./library_scripts.sh
 
 # nanolayer is a cli utility which keeps container layers as small as possible
 # source code: https://github.com/devcontainers-contrib/nanolayer
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
 # and if missing - will download a temporary copy that automatically get deleted at the end
 # of the script
-ensure_nanolayer nanolayer_location "v0.4.45"
+#ensure_nanolayer nanolayer_location "v0.4.45"
 
-$nanolayer_location \
-    install \
-    devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/apt-get-packages:1.0.4" \
-    --option packages='build-essential,autoconf,m4,libncurses5-dev,libwxgtk3.0-gtk3-dev,libwxgtk-webview3.0-gtk3-dev,libgl1-mesa-dev,libglu1-mesa-dev,libpng-dev,libssh-dev,unixodbc-dev,xsltproc,fop,libxml2-utils,libncurses-dev,openjdk-11-jdk,procps'
+#$nanolayer_location \
+#    install \
+#    devcontainer-feature \
+#    "ghcr.io/devcontainers-contrib/features/apt-get-packages:1.0.4" \
+#    --option packages='build-essential,autoconf,m4,libncurses5-dev,libwxgtk3.0-gtk3-dev,libwxgtk-webview3.0-gtk3-dev,libgl1-mesa-dev,libglu1-mesa-dev,libpng-dev,libssh-dev,unixodbc-dev,xsltproc,fop,libxml2-utils,libncurses-dev,openjdk-11-jdk,procps'
 
 KERL_BUILD_BACKEND=git
 OTP_GITHUB_URL='https://github.com/emqx/otp'
 version=`cat ../../.tool-versions | grep  erlang | awk '{print $2}'`
+echo "found erlang version: $version"
 
 $nanolayer_location \
     install \
     devcontainer-feature \
     "ghcr.io/devcontainers-contrib/features/asdf-package:1.0.8" \
-    --option plugin='erlang' --option version="$version"
+    --option plugin='erlang' --option version="$VERSION"
 
 echo 'Done!'
